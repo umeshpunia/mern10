cfa6b548b4d9722db19c59d3a1254ea679c13b66
umeshpunia <punia.umesh@gmail.com>
2021-10-25 14:36:09 +0530

    second

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const port = process.env.PORT || 1000;
-app.get('/', (req, res) => {
+app.get(`/${process.env.ROUTE}`, (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
